761d621cdc58228056abe178e9c786c0105de29d
tomas95go <tomas95go@gmail.com>
2022-06-11 17:21:33 -0300

    Basic pug setup

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, `\\assets\\`)));
+app.set(`view engine`, `pug`);
+
 app.get(`/`, (req, res) => {
-  res.send(`Hey there, how is it going?`);
+  res.render(`index`, {
+    title: `The Wire Characters`,
+  });
 });
 
 app.listen(PORT, () => {
