8728a5a62a7fb7956f8fedbbb8617b6feec25d70
tomas95go <tomas95go@gmail.com>
2022-06-14 23:28:01 -0300

    Specifying port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const BASE_URL = __dirname;
 const characters = require(`${BASE_URL}/routes/characters/characters`);
 
